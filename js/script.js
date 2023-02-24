cb82fe87bc0b969732a8d78692ed671d016b4d2a
danilopiccinni <danilopiccinni@gmail.com>
2023-02-24 16:40:24 +0100

    feat: dato dello stile ai singoli elementi

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
+let BodyEl = document.getElementById ('body')
+
+BodyEl.classList.add ('container' , 'pt-5')
 
 let UlEl = document.getElementById ("lista")
+
+UlEl.classList.add ("row" , 'row-cols-4')
+
 
 
 for (let i = 1 ; i <= 100 ; i++) {
@@ -8,21 +14,30 @@ for (let i = 1 ; i <= 100 ; i++) {
 
     UlEl.append(LiEl)
 
+    LiEl.style.listStyleType ='none'
+    LiEl.classList.add ('text-center', 'p-1' )
+
+
+
  
     if (i % 3 == 0) {
 
         LiEl.innerHTML = "Fizz"
+        LiEl.classList.add ('bg-primary')
 
         } else
             if ( i % 5 == 0) {
 
                 LiEl.innerHTML = "Buzz"
+                LiEl.classList.add ('bg-success')
 
             } else {
         
                 LiEl.innerHTML = "FizzBuzz"
+                LiEl.classList.add ('bg-danger')
 
             }
 
+            
 }
 
