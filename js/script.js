5385cda0277d1a407ed07c30383dec9ce8f9831b
danilopiccinni <danilopiccinni@gmail.com>
2023-02-26 18:45:19 +0100

    feat: aggiunto stile con e senza boostrap
    per fare pratica...e divertimento

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 let BodyEl = document.getElementById ('body')
 
-BodyEl.classList.add ('container' , 'pt-5')
+BodyEl.classList.add ( 'p-5' , 'd-flex' , 'justify-content-center')
+BodyEl.style.backgroundColor = '#031322'
+
 
 let UlEl = document.getElementById ("lista")
 
-UlEl.classList.add ("row" , 'row-cols-4')
-
+UlEl.style.maxWidth = '1500px'
+UlEl.style.textAlign = 'center'
+UlEl.classList.add ("row" , 'd-flex' , 'justify-content-center' , 'p-5')
 
 
 for (let i = 1 ; i <= 100 ; i++) {
@@ -15,26 +18,30 @@ for (let i = 1 ; i <= 100 ; i++) {
     UlEl.append(LiEl)
 
     LiEl.style.listStyleType ='none'
-    LiEl.classList.add ('text-center', 'p-1' )
+    LiEl.classList.add ('p-1' , 'm-3' , 'd-flex' , 'align-items-center' , 'justify-content-center' , 'border' , 'border-dark-subtle' , 'rounded-3')
+    LiEl.style.height = '150px'
+    LiEl.style.width = '150px'
+    LiEl.style.fontSize ='20px'
+    
 
-
-
- 
     if (i % 3 == 0) {
 
         LiEl.innerHTML = "Fizz"
-        LiEl.classList.add ('bg-primary')
+
+        LiEl.style.backgroundColor ='#ff0000'
 
         } else
             if ( i % 5 == 0) {
 
                 LiEl.innerHTML = "Buzz"
-                LiEl.classList.add ('bg-success')
+                // LiEl.classList.add ('bg-success')
+                LiEl.style.backgroundColor ='#00ff00'
 
             } else {
         
                 LiEl.innerHTML = "FizzBuzz"
-                LiEl.classList.add ('bg-danger')
+                // LiEl.classList.add ('bg-danger')
+                LiEl.style.backgroundColor ='#0011ff'
 
             }
 
